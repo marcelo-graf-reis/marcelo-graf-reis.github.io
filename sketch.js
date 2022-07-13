a369d9f7b0d476bb2e45e448d08c36246885a704
marcelo-graf-reis <109229577+marcelo-graf-reis@users.noreply.github.com>
2022-07-13 15:41:05 +0100

    Primeiro Upload
    Upload de portfolio

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function preload() {
+
+}
+
+
+function setup() {
+    createCanvas(displayWidth, displayHeight);
+    getAudioContext().resume();
+
+
+
+}
+
+
+function draw() {
+    background(255, 0, 0);
+
+}
